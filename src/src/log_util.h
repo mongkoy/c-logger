f01776215d4f2c53c4b1b55974edb0428e3e7aa3
jeftyneg <jeftyneg@gmail.com>
2011-01-30 06:25:15 +0000

    Changed comments

--- src/src/log_util.h
+++ src/src/log_util.h
@@ -2,12 +2,12 @@
  * c-logger - copyright 2011, Jefty E. Negapatan, jeftyneg@gmail.com
  * This file is part of c-logger.
  *
- * liblogger is free software; you can redistribute it and/or modify
+ * c-logger is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * liblogger is distributed in the hope that it will be useful,
+ * c-logger is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
