d3dd1805177070550678c32a2940bf2b466e0b0e
jeftyneg <jeftyneg@gmail.com>
2011-02-24 14:15:47 +0000

    Added FileProcessor which is a subclass of DataProcessor

---
+++ src/logserver/java/logview4j/src/org/logview4j/listener/FileProcessor.java
@@ -0,0 +1,37 @@
+package org.logview4j.listener;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * 
+ * @author Jefty Negapatan
+ */
+public class FileProcessor extends DataProcessor {
+
+	protected File file;
+
+	public FileProcessor(File file) {
+		this.file = file;
+	}
+
+	public void run() {
+		FileInputStream fileInputStream = null;
+		try {
+			fileInputStream = new FileInputStream(file);
+			readData(fileInputStream);
+		} catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		if (fileInputStream != null) {
+			try {
+				fileInputStream.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			fileInputStream = null;
+		}
+	}
+}
